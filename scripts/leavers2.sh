7183c631738b34b2ed56a45024dfb09a1b8e61f9
intetunder <sander.hvas@gmail.com>
2017-03-07 00:12:59 +0000

    Update leavers2.sh

--- scripts/leavers2.sh
+++ scripts/leavers2.sh
@@ -6,6 +6,6 @@ FILENEW=leaversexcel`date +%Y-%m-%d`.csv
 read -e -p "Enter filename, use tab for completion: " file
 
 ## Run the formatting and save it to a new file
-sed 's/","/ /3' "$file" | sed 's/","/ /3' | sed 's/" "/ /g'  | grep "Leaver" | awk '{print $6,$7,$8,$9,$10}'  > /tmp/$FILENEW
+sed 's/","/,/3' "$file" | sed 's/","/,/3' | sed 's/" "/ /g'  | grep "Leaver" | awk '{print $6,$7,$8,$9,$10}'  > /tmp/$FILENEW
 
 echo "The formatted file, is created: /tmp/$FILENEW, please import it into: LINK"
